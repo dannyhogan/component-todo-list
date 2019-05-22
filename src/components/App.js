b660a24ed27ca697448c17d5a4ea3ca5826df8cc
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-22 09:42:06 -0700

    Imported todo data and created base App class

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
 import Component from './Component.js';
+import todos from '../../data/todo-data.js';
+
+class App extends Component {
+    
+    render() {
+        
+    }
+    
+    renderTemplate() {
+        return /*html*/`
+            <div>
+                <main></main>
+            </div>
+        `;
+    }
+}
+
+export default App;
