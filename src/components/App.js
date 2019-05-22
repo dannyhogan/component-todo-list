6b00ef2c5cd0d92d1c58cc62c47745799000a51d
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-22 09:53:15 -0700

    Added todoList to render() in App component, passed the data down to list component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import Component from './Component.js';
+import TodoList from './TodoList.js';
 import todos from '../../data/todo-data.js';
 
 class App extends Component {
     
     render() {
-        
+        const dom = this.renderDOM();
+
+        const todoList = new TodoList({ todos });
+        const todoListDOM = todoList.render();
+
+        const main = dom.querySelector('main');
+        main.appendChild(todoListDOM);
+
+        return dom;
     }
     
     renderTemplate() {
