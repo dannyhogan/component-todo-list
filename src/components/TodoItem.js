a71b28474466089d851dbb06f107e0765454476f
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-22 10:22:40 -0700

    Added render methon on TodoItem  and conditional statement to check if todo has been completed

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,13 +2,24 @@ import Component from './Component.js';
 
 class TodoItem extends Component {
 
-    render() {}
+    render() {
+        const item = this.renderDOM();
+
+        return item;
+    }
 
     renderTemplate() {
+
+        let checked = '';
+
+        if(this.props.todo.completed) {
+            checked = 'checked';
+        }
+
         return /*html*/`
         <li>
-            <label>Todo
-                <input type="checkbox">
+            <label>${this.props.todo.task}
+                <input type="checkbox" ${checked}>
             </label>
         </li>
         `;
