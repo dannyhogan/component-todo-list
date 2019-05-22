b9af280b1ec6bb7770f0a4a05dcdf5eb2fd49c8f
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-22 09:42:24 -0700

    Created ToDoList component and basic renderTemplate

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import Component from './Component.js';
+
+class ToDoList extends Component {
+    
+    renderTemplate() {
+        return /*html*/`
+        <ul></ul>
+    `;
+    }
+}
+
+export default ToDoList;
