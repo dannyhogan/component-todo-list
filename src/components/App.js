db0ba1c7a15e174169307fff13b0f9482cf6faae
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-22 10:33:19 -0700

    Imported Header component and appended to main in render method

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import Component from './Component.js';
 import TodoList from './TodoList.js';
+import Header from './Header.js';
 import todos from '../../data/todo-data.js';
 
 class App extends Component {
@@ -7,10 +8,14 @@ class App extends Component {
     render() {
         const dom = this.renderDOM();
 
+        const header = new Header();
+        const headerDOM = header.render();
+
         const todoList = new TodoList({ todos });
         const todoListDOM = todoList.render();
 
         const main = dom.querySelector('main');
+        main.appendChild(headerDOM);
         main.appendChild(todoListDOM);
 
         return dom;
