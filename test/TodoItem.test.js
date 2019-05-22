03baab6b54fc33e68f341d0a1d647763788fc8c4
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-22 10:34:58 -0700

    Added second test for uncompleted todo item

--- test/TodoItem.test.js
+++ test/TodoItem.test.js
@@ -3,7 +3,7 @@ const test = QUnit.test;
 
 QUnit.module('todo item');
 
-test('returns correct dom from renderTemplate', assert => {
+test('returns completed TodoItem dom', assert => {
     //arrange
     const todo = {
         task: 'Learn HTML',
@@ -14,6 +14,29 @@ test('returns correct dom from renderTemplate', assert => {
         <li>
             <label>Learn HTML
                 <input type="checkbox" checked>
+            </label>
+        </li>
+    `;
+
+    //act
+    const todoItem = new TodoItem({ todo });
+    const todoItemDOM = todoItem.renderTemplate();
+
+    //assert
+    assert.htmlEqual(todoItemDOM, expected);
+});
+
+test('returns uncompleted TodoItem dom', assert => {
+    //arrange
+    const todo = {
+        task: 'Learn HTML',
+        completed: false
+    };
+
+    const expected = /*html*/`
+        <li>
+            <label>Learn HTML
+                <input type="checkbox" >
             </label>
         </li>
     `;
