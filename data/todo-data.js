2a222561cfab9137f111edba6c777307f38d1078
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-22 09:32:49 -0700

    Added initial file structure

---
+++ data/todo-data.js
@@ -0,0 +1,36 @@
+const todos = [
+    {
+        task: 'Learn HTML',
+        completed: true
+    },
+    {
+        task: 'Learn CSS',
+        completed: true
+    },
+    {
+        task: 'Learn Javascript',
+        completed: true
+    },
+    {
+        task: 'Learn React',
+        completed: false
+    },
+    {
+        task: 'Learn Firebase',
+        completed: false
+    },
+    {
+        task: 'Learn Node',
+        completed: false
+    },
+    {
+        task: 'Learn ASync',
+        completed: false
+    },
+    {
+        task: 'Learn APIs',
+        completed: false
+    }
+];
+
+export default todos;
