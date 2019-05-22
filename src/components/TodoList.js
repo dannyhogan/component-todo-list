9caa2036af64b81347f0522e6141f0718e4ec202
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-22 10:22:05 -0700

    Added render method to TodoList component, created TodoItem for each todo in data

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,27 @@
 import Component from './Component.js';
+import TodoItem from './TodoItem.js';
 
 class ToDoList extends Component {
     
+    render() {
+        const list = this.renderDOM();
+
+        const todos = this.props.todos;
+
+        todos.forEach(todo => {
+            const todoItem = new TodoItem({ todo });
+            const todoItemDOM = todoItem.render();
+            console.log(todoItemDOM);
+            list.appendChild(todoItemDOM);
+        });
+
+        return list;
+    }
+
     renderTemplate() {
         return /*html*/`
-        <ul>${this.props.todos.length}</ul>
-    `;
+            <ul></ul>
+        `;
     }
 }
 
