be8a8d04e7a323d0b29801c45eadbb7fe013fe98
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-23 15:04:54 -0700

    Created todoAPI and imported findByID function

---
+++ src/services/todo-api.js
@@ -0,0 +1,25 @@
+import findById from './find-by-id.js';
+
+const todoApi = {
+    key: 'todo',
+    save(todo) {
+        let todos = todoApi.getAll();
+        todos.push(todo);
+        const todoData = JSON.stringify(todos);
+        localStorage.setItem(todoApi.key, todoData);
+    },
+    get(id) {
+        const todos = todoApi.getAll();
+        return findById(todos, id);
+    },
+    getAll() {
+        const todoData = localStorage.getItem(todoApi.key);
+        let todos = JSON.parse(todoData);
+        if(!todos) {
+            todos = [];
+        }
+        return todos;
+    }
+};
+
+export default todoApi;
