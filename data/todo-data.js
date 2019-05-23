0690eed3000d67ecf3ef03d3dcea881748670a49
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-23 15:03:45 -0700

    Added unique ID to each todo in data

--- data/todo-data.js
+++ data/todo-data.js
@@ -1,25 +1,31 @@
 const todos = [
     {
+        id: 'todo1',
         task: 'Learn HTML',
         completed: true
     },
     {
+        id: 'todo2',
         task: 'Learn CSS',
         completed: true
     },
     {
+        id: 'todo3',
         task: 'Learn Javascript',
         completed: false
     },
     {
+        id: 'todo4',
         task: 'Learn React',
         completed: false
     },
     {
+        id: 'todo5',
         task: 'Learn Firebase',
         completed: false
     },
     {
+        id: 'todo6',
         task: 'Learn Node',
         completed: false
     }
