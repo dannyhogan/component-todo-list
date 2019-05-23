acbc6d6f9474abda3c9c595f739f70a6f7559077
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-23 09:25:47 -0700

    Added event listener to input for filter and used callback function in Filter component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,15 +4,28 @@ class Filter extends Component {
     
     render() {
         const dom = this.renderDOM();
+        const input = dom.querySelector('#textFilter');
+
+        input.addEventListener('input', () => {
+            this.props.onFilter({ 
+                task: input.value
+            });
+        });
 
         return dom;
     }
     
     renderTemplate() {
         return /*html*/`
-            <section>
-                <input type="text" id="filter" placeholder="Search todo list:">
-            </section>
+            <div id="filters">
+                <section>
+                    <input type="text" id="textFilter" placeholder="Search todo list:">
+                </section>
+                <section>
+                    <input type="checkbox">Complete
+                    <input type="checkbox">Incomplete
+                </section>
+            </div>
         `;
     }
 }
